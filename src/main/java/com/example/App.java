a7719196d64bbe6881fd787dcd8513f0ed867dc0
Amritya <ascreen.go143@gmail.com>
2024-10-14 13:47:15 +0530

    second

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -1,7 +1,7 @@
 package com.example;
 
 public class App {
-    public static void main(String[] args) {
-        System.out.println("Hello, Maven!");
-    }
+ public static void main(String[] args) {
+  System.out.println("Hello, Maven!");
+ }
 }
